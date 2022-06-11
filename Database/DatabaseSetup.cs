cd43e312254853f6753546a83ae2dc675ef8bf28
hysabelly <90413604+hysabelly@users.noreply.github.com>
2022-06-10 22:33:54 -0300

    Update DatabaseSetup.cs

--- Database/DatabaseSetup.cs
+++ Database/DatabaseSetup.cs
@@ -4,27 +4,47 @@ namespace LabManager.Database;
 
 class DatabaseSetup
 {
-    private DatabaseConfig databaseConfig; 
+    private DatabaseConfig databaseConfig;
 
     public DatabaseSetup(DatabaseConfig databaseConfig)
     {
-        this.databaseConfig=databaseConfig;
-        CreateComputerTable();
+        this.databaseConfig = databaseConfig;
+        CreateTableComputer();
+        CreateTableLab();
     }
-    public void CreateComputerTable()
+
+    private void CreateTableComputer()
     {
         var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
+
         var command = connection.CreateCommand();
         command.CommandText = @"
             CREATE TABLE IF NOT EXISTS Computers(
-                id int not null primary key, 
+                id int not null primary key,
                 ram varchar(100) not null,
-                processador varchar(100) not null
+                processor varchar(100) not null
             );
         ";
         command.ExecuteNonQuery();
+        connection.Close();
+    }
 
+    private void CreateTableLab()
+    {
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Labs(
+                id int not null primary key,
+                number varchar(100) not null,
+                name varchar(100) not null,
+                block varchar(100) not null
+            );
+        ";
+        command.ExecuteNonQuery();
         connection.Close();
     }
 }
