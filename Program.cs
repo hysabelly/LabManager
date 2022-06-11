160e9bc0035ed29f92bb1ddb99a58c42226fccbe
hysabelly <90413604+hysabelly@users.noreply.github.com>
2022-06-10 22:39:45 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -71,30 +71,44 @@ if(modelName == "Computer")
     }
 }
 
-if(modelName == "Lab")
+else if(modelName == "Lab")
 {
     if(modelAction == "List")
     {
-        
+        Console.WriteLine("Lab List");
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Labs;";
+
+        var reader = command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+        }
+
+        reader.Close();
+        connection.Close();
     }
 
     if(modelAction == "New")
     {
+        var id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        var name = args[4];
+        var block = args[5];
+
         var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
 
-        Console.WriteLine("New lab");
-        int id = Convert.ToInt32(args[2]);
-        string num = args[3];
-        string nome = args[4];
-        string bloco = args[5];
-         
         var command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Lab VALUES($id, $num,  $nome, $bloco);";
+        command.CommandText = "INSERT INTO Labs VALUES($id, $number, $name, $block);";
         command.Parameters.AddWithValue("$id", id);
-        command.Parameters.AddWithValue("$num", num);
-        command.Parameters.AddWithValue("$nome", nome);
-        command.Parameters.AddWithValue("$bloco", bloco);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
 
         command.ExecuteNonQuery();
         connection.Close();
